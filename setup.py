8a87e49e9028ebfc90b72fd95b1a17b46dd6c4f2
joseph <61469887+karianjahi@users.noreply.github.com>
2022-07-26 11:18:30 +0200

    Add setup.py to allow pip-installation of package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+setup(
+    name="poblano_count_words_app",
+    version="0.0.1",
+    description="package that counts words in a text (string)",
+    author="NLpoblano cohort",
+    author_email="nlpoblano@poblano.com",
+    packages=["poblano_count_words_app"],
+    url = "git@github.com:karianjahi/poblano_count_words_app.git",
+    classifiers = [
+        "Programming Language :: python :: 3.9.7",
+    ],
+)
